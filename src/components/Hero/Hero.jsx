3e1567c1825923e91f60d019d47c34c366d2f072
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2023-11-09 14:35:08 +0200

    hero clear

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,56 +1,15 @@
 import React from 'react';
-import {
-  LinkBtn,
-  MainContainer,
-  ReservedBtn,
-  SliderContainer,
-  SliderContent,
-  SliderItemOne,
-  SliderItemThree,
-  SliderItemTwo,
-  SliderText,
-  SliderTitle,
-} from './Hero.styled';
-import { CarTitleBlue } from '../ListItem/ListItem.styled';
+import { Btn, Container, LinkBtn, Title } from './Hero.styled';
 
 const Hero = () => {
-  const settings = {
-    fade: true,
-    speed: 2000,
-    autoplaySpeed: 500,
-    infinite: true,
-    autoplay: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    pauseOnHover: false,
-  };
-
   return (
     <>
-      <MainContainer>
-        <SliderContent>
-          <SliderTitle>
-            Unlock the road to adventure with our{' '}
-            <CarTitleBlue>Car Rental Service</CarTitleBlue>!
-          </SliderTitle>
-
-          <SliderText>
-            Rent your best car and enjoy your awesome trip.
-          </SliderText>
-
-          <ReservedBtn>
-            <LinkBtn to="/catalog">Reserve Now</LinkBtn>
-          </ReservedBtn>
-        </SliderContent>
-
-        <SliderContainer {...settings}>
-          <SliderItemOne />
-
-          <SliderItemTwo />
-
-          <SliderItemThree />
-        </SliderContainer>
-      </MainContainer>
+      <Container>
+        <Title>TIttle</Title>
+        <Btn>
+          <LinkBtn to="/catalog">Reserve Now</LinkBtn>
+        </Btn>
+      </Container>
     </>
   );
 };
--- src/components/Hero/Hero.styled.jsx
+++ src/components/Hero/Hero.styled.jsx
@@ -1,103 +1,34 @@
 import styled from 'styled-components';
-import Slider from 'react-slick'
-import highlander from '../../images/highlanderw.png'
-import volk from '../../images/volk.png'
-import car from '../../images/car.png'
-
-import pattern from '../../images/pattern.svg';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { theme } from '../../styles/theme';
 
-
-export const MainContainer = styled.div`
-margin-left: auto;
-margin-right: auto;
-margin-top: 70px;
-display: flex;
-justify-content: space-between;
-// align-items: baseline;
-`
-
-
-
-export const SliderItemOne = styled.div`
-    width: 500px;
-    height: 500px;
-    background: url(${highlander});
-    background-position: center;
-    background-repeat: no-repeat;
-    block-size: contain;
+export const Container = styled.div`
+  margin-top: 85px;
+  display: flex;
+  flex-direction: column;
 `;
 
-export const SliderItemTwo = styled.div`
-width: 500px;
-height: 500px;
-background:  
-    url(${car});
-    background-position: center;
-    background-repeat: no-repeat;
-    block-size: contain;
-`
-
-export const SliderItemThree = styled.div`
-width: 800px;
-height: 500px;
-background: 
-    url(${volk});
-    background-position: center;
-    background-repeat: no-repeat;
-    block-size: contain;
-`
-
-
-
-export const SliderContainer = styled(Slider)`
-overflow: hidden;
-width: 800px;
-height: 650px;
-background: 
-    url(${pattern});
-    background-position: center;
-    background-repeat: no-repeat;
-    block-size: contain;
-}
+export const Title = styled.h1`
+  width: 600px;
+  font-size: 45px;
+  font-weight: 700;
+  margin-bottom: 50px;
+  color: ${theme.colors.black};
 `;
 
-export const SliderContent = styled.div`
-    margin-top: 60px;
-    margin-left: 130px;
-`
+export const Btn = styled.button`
+  background-color: #3470ff;
+  width: 200px;
+  padding: 12px 50px;
+  border: none;
+  border-radius: 12px;
 
-export const SliderTitle = styled.h1`
-    width: 600px;
-    font-size: 68px;
-    font-weight: 700;
-    margin-bottom: 50px;
-    color: ${theme.colors.black};
-`
-
-export const SliderText = styled.p`
-color: rgba(18, 20, 23, 0.50);
-font-size: 32px;
-font-style: normal;
-font-weight: ${theme.fontWeight.regular};
-// line-height: 18px;
-margin-bottom: 50px;
-`
-
-export const ReservedBtn = styled.button`
-    background-color: #3470FF;
-    padding: 12px 50px;
-    border: none;
-    outline: none;
-    border-radius: 12px;
-
-    &:hover{
-        background-color: #0B44CD;
-    }
-`
+  &:hover {
+    background-color: #0b44cd;
+  }
+`;
 export const LinkBtn = styled(Link)`
-color: #fff;
-text-decoration: none;
-    font-weight: 600;
-`
+  color: #fff;
+  text-decoration: none;
+  font-weight: 600;
+`;
